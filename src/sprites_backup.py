9958f84da1b565ec698240a267b28ca2d74e3b3b
Adrian Pineau <adrian.pineau@gmail.com>
2022-05-10 23:14:22 +0200

    need to add elastic collisions

--- src/sprites_backup.py
+++ src/sprites_backup.py
@@ -28,7 +28,14 @@ class Player(pg.sprite.Sprite):
         self.rect.center = self.pos
         
     def update(self):
-        self.vel.x = 0
+        balls = pg.sprite.Group()
+        for sprite in self.game.balls:
+            if sprite != self:
+                balls.add(sprite) 
+        # Checking if objets are colliding already
+        previous_collisions = pg.sprite.spritecollide(self, balls, False, pg.sprite.collide_circle)
+        
+        self.vel = vec(2, 0)
         self.acc = vec(0, PLAYER_GRAVITY)
         keys = pg.key.get_pressed()
         if keys[pg.K_RIGHT]:
@@ -47,62 +54,55 @@ class Player(pg.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
             self.pos.x = self.rect.centerx
-            self.vel.x *= -1
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
             self.pos.x = self.rect.centerx
-            self.vel.x *= -1
     
-        # x-collisions with platforms
-        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-        self.x_collisions_platforms(hits)
+        # # x-collisions with platforms
+        # hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        # if hits:
+        #     # Hitting left border of a platform
+        #     if self.vel.x > 0:
+        #         self.rect.right = hits[0].rect.left
+        #         self.pos.x = self.rect.centerx
+        #     # Hitting right border of a platform
+        #     elif self.vel.x < 0:
+        #         self.rect.left = hits[0].rect.right
+        #         self.pos.x = self.rect.centerx
 
         # Updating y pos
         self.pos.y += self.vel.y + 0.5 * self.acc.y
         self.rect.centery = self.pos.y
 
         # y-collisions with platforms
-        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-        self.y_collisions_platforms(hits)
-        
-        hits = pg.sprite.spritecollide(self, self.game.balls, False, pg.sprite.collide_circle)  
-        if hits:
-            self.ball_collision(hits[0])
-        
-                  
-    def x_collisions_platforms(self, hits):
-        if hits:
-            # Hitting left border of a platform
-            if self.vel.x > 0:
-                self.rect.right = hits[0].rect.left
-                self.pos.x = self.rect.centerx
-            # Hitting right border of a platform
-            elif self.vel.x < 0:
-                self.rect.left = hits[0].rect.right
-                self.pos.x = self.rect.centerx
-    
-    def y_collisions_platforms(self, hits):
-        if hits:
-            # Falling, hitting top of a platform
-            if self.vel.y > 0:
-                self.rect.bottom = hits[0].rect.top
-                self.pos.y = self.rect.centery
-                self.vel.y *= 0
-            # Jumping, hitting bottom of a platform
-            elif self.vel.y < 0:
-                self.rect.top = hits[0].rect.bottom
-                self.pos.y = self.rect.centery
-                self.vel.y = 0
+        # hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        # if hits:
+        #     # Falling, hitting top of a platform
+        #     if self.vel.y > 0:
+        #         self.rect.bottom = hits[0].rect.top
+        #         self.pos.y = self.rect.centery
+        #         self.vel.y *= 0
+        #     # Jumping, hitting bottom of a platform
+        #     elif self.vel.y < 0:
+        #         self.rect.top = hits[0].rect.bottom
+        #         self.pos.y = self.rect.centery
+        #         self.vel.y = 0
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
             self.pos.y = self.rect.centery
             self.vel.y = 0
             
+        new_collisions = pg.sprite.spritecollide(self, balls, False, pg.sprite.collide_circle)
+        if new_collisions and not previous_collisions:
+            self.ellastic_collision(self.game.player)
+            
     def standing_on_platform(self) -> bool:
         self.rect.y += 1
-        on_floor = self.rect.bottom > HEIGHT
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)   
         self. rect.y -= 1
-        return on_floor
+        if hits:
+            return True
+        return False
        
     def jump(self) -> None:  
         if self.standing_on_platform(): # Can only jump on platforms
@@ -113,27 +113,22 @@ class Player(pg.sprite.Sprite):
         new_velx_p2 = ((2 * self.m * self.vel.x) - (self.m - p2.m) * p2.vel.x) / (self.m + p2.m)
         self.vel.x = new_velx_p1
         p2.vel.x = new_velx_p2
-        
-    def ball_collision(self, p2):
-        if self.vel.x > 0:
-            self.rect.right = p2.rect.left
-            self.pos.x = self.rect.centerx
-        if self.vel.x < 0:
-            self.rect.left = p2.rect.right
-            self.pos.x = self.rect.centerx
-            
-
-# --------
+ 
+# --------------------------------------------------------------
 # BallGame
 
 class BallGame(Player):
     def __init__(self, game, x, y, color, velocity):
         super().__init__(game, x, y, color)
-        self.acc = vec(0, BALL_GRAVITY)
+        self.acc = vec(0, 0)
         self.vel = vec(velocity)
 
     def update(self):       
-        previous_collisions = pg.sprite.spritecollide(self, self.game.players, False, pg.sprite.collide_circle)
+        balls = pg.sprite.Group()
+        for sprite in self.game.balls:
+            if sprite != self: balls.add(sprite) 
+        # Checking if objets are colliding already
+        previous_collisions = pg.sprite.spritecollide(self, balls, False, pg.sprite.collide_circle)
          
         # Updating velocity
         self.vel += self.acc
@@ -148,17 +143,11 @@ class BallGame(Player):
         # Collision with ceiling
         elif self.rect.top < 0:
             self.vel.y *= -1
-        # Left and right borders
-        if self.rect.left < 0:
-            self.rect.left = 0
-            self.pos.x = self.rect.centerx
-            self.vel.x *= -1
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-            self.pos.x = self.rect.centerx
+        # Collision with left/right borders
+        elif self.rect.left < 0 or self.rect.right > WIDTH:
             self.vel.x *= -1
             
-        ## Collision with the net and the moving platform
+        # Collision with the net and the moving platform
         # current_collisions = pg.sprite.spritecollide(self, obstacles, False)
         # if current_collisions:
         #     obstacle = current_collisions[0]
@@ -177,13 +166,24 @@ class BallGame(Player):
         #         self.vel.x *= -1
         
         # Collision with players
-        hits = pg.sprite.spritecollide(self, self.game.players, False, pg.sprite.collide_circle)
-        if hits:
-            if hits[0].vel.x == 0:
-                self.vel.x *= -1
-            else:
-                self.ellastic_collision(hits[0])
+        new_collisions = pg.sprite.spritecollide(self, balls, False, pg.sprite.collide_circle)
+        if new_collisions and not previous_collisions:
+            self.ellastic_collision(new_collisions[0])
         
+
+        # 2
+        # v_ball_game = vec(self.rect.x, self.rect.y)
+        # v_player = vec (self.game.player.rect.x, self.game.player.rect.y)
+        # r_ball_game = self.radius
+        # r_player = self.game.player.radius
+        # if v_ball_game.distance_to(v_player) < r_ball_game + r_player:
+        #     print("Hit")
+        #     new_vec = v_player - v_ball_game
+        #     self.vel = vec(self.vel.x, self.vel.y).reflect(new_vec)
+        #     print(self.vel)
+            # self.rect.bottom = self.game.player.rect.top
+            # self.pos.y = self.rect.centery
+            
 
 # --------------------------------------------------------------
 # Platform
